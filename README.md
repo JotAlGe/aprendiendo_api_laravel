d9ccaf758230f10ff9667f74fb768c98cb8ccd74
Juanchis <juanchismo10@gmail.com>
2022-06-03 14:27:46 -0300

    Inicio del proyecto para aprender a crear una api en laravel

---
+++ README.md
@@ -0,0 +1,14 @@
+# Api de Usuarios en Laravel 9
+
+## Sobre la Api
+
+Primera API en Laravel. Es un CRUD de usuarios.
+
+### Link de la Api
+
+- **[Api de Usuarios](https://vehikl.com/)**
+
+
+## License
+
+The Laravel framework is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Http\Resources\UserResource;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return UserResource::collection(User::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //return new UserResource(User::create($request->all()));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return new UserResource(User::findOrFail($id));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        return new UserResource(User::findOrFail($id));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return new UserResource(User::findOrFail($id));
+    }
+}
